ba4bf2f6a837745af888ac4a85162aa02f73eb79
Natanael <natanaelfs@gmail.com>
2016-05-17 11:08:43 -0300

    fluxo provisorio estoque

--- Control.DAL/Data/DBConexao.cs
+++ Control.DAL/Data/DBConexao.cs
@@ -50,7 +50,7 @@ namespace Control.DAL.Data
                 case 3:
 
                     //Conexão de produção Gtwave 
-                    strConn = @"Data Source=192.99.149.206;Initial Catalog=dbControl;Persist Security Info=True;User ID=nfs_control;Password=Il1cy$16";
+                    strConn = @"Data Source=192.99.149.206;Initial Catalog=dbControl;Persist Security Info=True;User ID=nfs_control;Password=vDy20v3#";
                     break;
 
                 case 4:
