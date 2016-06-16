b958d4ec071ac44380f0909af59901ef6a8402fd
Uriel Silva <urielbr@gmail.com>
2016-06-16 18:00:51 -0300

    Alterações item de correção 9

---
+++ Control.DAL/Migrations/201606161844013_1506_migration.cs
@@ -0,0 +1,16 @@
+namespace Control.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _1506_migration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
