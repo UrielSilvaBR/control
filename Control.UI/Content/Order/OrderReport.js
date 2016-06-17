6966f3e668e8ad2baa060ef2cd218cc31512ca77
GTWave <gtwave@hotmail.com>
2016-06-17 02:08:05 -0300

    Alterações Invoice Item

---
+++ Control.UI/Content/Order/OrderReport.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+});
+
+function ConfirmarPedido(idPedido) {
+    alert('teste');
+
+    waitingDialog.show('Proposta confirmada', { dialogSize: 'sm', progressType: 'success' });
+
+    setTimeout(function () {
+        $.ajax({
+            url: '/Pedido/ConfirmarProposta',
+            data: { OrderID: idPedido },
+            type: 'POST',
+            success: function (result) {
+                waitingDialog.hide();
+                ShowSuccess(result);
+            }
+        });
+    }, 1000);
+}
